830d8763c5eb224c63fd518250d13a5443b81868
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-04-22 23:06:51 -0400

    Create App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Bracket from './components/Bracket'
+
+const uuidv4 = require('uuid/v4');
+
+
+const teams = [
+  [
+    {'top': 'Duke', 'bottom': 'Albany', 'winnerPos': 'top'},
+    {'top': 'Colorado State', 'bottom': 'Harvard', 'winnerPos': 'top'},
+    {'top': 'Oklahoma State', 'bottom': 'Oregon', 'winnerPos': 'bottom'},
+    {'top': 'Saint Louis', 'bottom': 'New Mexico State', 'winnerPos': 'top'},
+    {'top': 'Memphis', 'bottom': 'St. Mary\'s', 'winnerPos': 'top'},
+    {'top': 'Michigan State', 'bottom': 'Valparaiso', 'winnerPos': 'top'},
+    {'top': 'Creighton', 'bottom': 'Cincinnati', 'winnerPos': 'top'},
+  ],
+  [
+    {'top': 'Lousville', 'bottom': 'Colorado State', 'winnerPos': 'bottom'},
+    {'top': 'Oklahoma State', 'bottom': '', 'winnerPos': 'top'},
+    // {'top': 'Memphis', 'bottom': 'Michigan State', 'winnerPos': 'top'},
+    // {'top': 'Creighton', 'bottom': 'Duke', 'winnerPos': 'top'},
+  ],
+  // [
+  //   {'top': 'Lousville', 'bottom': 'Oklahoma State', 'winnerPos': 'top'},
+  //   {'top': 'Memphis', 'bottom': 'Creighton', 'winnerPos': 'top'},
+  // ],
+  // [
+  //   {'top': 'Lousville', 'bottom': 'Memphis'},
+  // ],
+]
 
 class App extends Component {
+  state = {
+    firstRoundTeams: [
+      {uuid: uuidv4(), value: ""}
+    ],
+    isDeleting: false,
+    isPreview: false
+  };
+
+  addFirstRoundTeam = () => {
+    let { firstRoundTeams } = this.state;
+    firstRoundTeams.push({uuid: uuidv4(), value: ""});
+    this.setState({ firstRoundTeams })
+  }
+
+  removeFirstRoundTeam = uuid => {
+    let { firstRoundTeams } = this.state;
+    firstRoundTeams = firstRoundTeams.filter(team => team.uuid !== uuid);
+    this.setState({ firstRoundTeams });
+  }
+
+  updateTeamName = (e, uuid) => {
+    let { firstRoundTeams } = this.state;
+    firstRoundTeams = firstRoundTeams.map(team => {
+      if (team.uuid === uuid){
+        team.value = e.target.value;
+      }
+      return team;
+    });
+    this.setState({ firstRoundTeams });
+  }
+
+  _organizeTeams(){
+    let teams = this.state.firstRoundTeams.map(x => x.value);
+    let rounds = []
+    for(let i=0; i < teams.length;){
+      let dict = {"top": teams[i]||""};
+      if (i+1 < teams.length) {dict["bottom"] = teams[i+1]||"";}
+      if (i+1 >= teams.length) {dict["winnerPos"] = "top"}
+      console.log(dict);
+      rounds.push(dict)
+      i += 2;
+    }
+    return rounds;
+  }
+
   render() {
+    let { firstRoundTeams, isDeleting, isPreview } = this.state;
+    console.log(this._organizeTeams());
+    if (isPreview) {
+      return <Bracket teams={ [this._organizeTeams()] } title={ "March Madness 2019" } />
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <div style={{ width: "60%", height: "80%", marginLeft: "30%", marginTop: "5%" }}>
+          <div className={ "flex-column" }>
+            <label htmlFor="title">Bracket Title</label>
+            <input type="text" maxLength="22" id="title" placeholder="Bracket Title" />
+          </div>
+          <div className={ "flex-column" }>
+            <div className={ "team-label-container" }>
+              <label htmlFor="title">Teams</label>
+              <button type="button" onClick={ this.addFirstRoundTeam } className={ "button button-positive" } >+</button>
+              <button type="button" onClick={ () => this.setState({isDeleting:!isDeleting}) } className={ "button button-negative" } >-</button>
+              <button type="button" onClick={ () => this.setState({isPreview:true}) } className={ "button button-preview" }>Preview</button>
+            </div>
+
+            { firstRoundTeams.map((entry, idx) => {
+              return <input type="text" style={{ display: "none" }} key={ entry.uuid } onChange={ val => this.updateTeamName(val, entry.uuid) } onClick={isDeleting ? () => this.removeFirstRoundTeam(entry.uuid) : () => {} } maxLength="22" id="title" placeholder={ `Team ${idx+1}`} />
+            }) }
+          </div>
+        </div>
+        <Bracket teams={ [this._organizeTeams()] } title={ "March Madness 2019" } />
+      </React.Fragment>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Bracket from './components/Bracket'
+import App from './App';
+import Bracket from './components/Bracket';
 import * as serviceWorker from './serviceWorker';
 
 const root = document.getElementById('root');
-const teams = [
-  [
-    {'top': 'Duke', 'bottom': 'Albany', 'winnerPos': 'top'},
-    {'top': 'Colorado State', 'bottom': 'Harvard', 'winnerPos': 'top'},
-    {'top': 'Oklahoma State', 'bottom': 'Oregon', 'winnerPos': 'bottom'},
-    {'top': 'Saint Louis', 'bottom': 'New Mexico State', 'winnerPos': 'top'},
-    {'top': 'Memphis', 'bottom': 'St. Mary\'s', 'winnerPos': 'top'},
-    {'top': 'Michigan State', 'bottom': 'Valparaiso', 'winnerPos': 'top'},
-    {'top': 'Creighton', 'bottom': 'Cincinnati', 'winnerPos': 'top'},
-  ],
-  [
-    {'top': 'Lousville', 'bottom': 'Colorado State', 'winnerPos': 'bottom'},
-    {'top': 'Oklahoma State', 'bottom': '', 'winnerPos': 'top'},
-    // {'top': 'Memphis', 'bottom': 'Michigan State', 'winnerPos': 'top'},
-    // {'top': 'Creighton', 'bottom': 'Duke', 'winnerPos': 'top'},
-  ],
-  // [
-  //   {'top': 'Lousville', 'bottom': 'Oklahoma State', 'winnerPos': 'top'},
-  //   {'top': 'Memphis', 'bottom': 'Creighton', 'winnerPos': 'top'},
-  // ],
-  // [
-  //   {'top': 'Lousville', 'bottom': 'Memphis'},
-  // ],
-]
 
-
-ReactDOM.render(<Bracket teams={ teams } title={ "March Madness 2019" } />, root);
+ReactDOM.render(<App />, root);
 
 serviceWorker.unregister();
