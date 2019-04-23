81d992b9d9198e55c733c9f25aa5666b477c4787
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-04-22 23:10:08 -0400

    Create function to generate arbitrary bracket layout based on number of teams

--- src/components/Bracket.jsx
+++ src/components/Bracket.jsx
@@ -3,6 +3,44 @@ import Match from './Match'
 
 const uuidv4 = require('uuid/v4');
 
+function generateBracket(teamCount) {
+  let round = 1;
+  let layout = {};
+  let exceptions = {
+      0: {1: 0},
+      2: {1: 1},
+      4: {1: 2, 2: 1},
+      6: {1: 3, 2: 2, 3: 1},
+      8: {1: 4, 2: 2, 3: 1},
+  };
+
+  if (teamCount < 0) {
+    return {1: 0};
+  }
+
+  if (teamCount % 2 !== 0) {
+    teamCount++;
+  }
+
+  if (exceptions[teamCount]) {
+    return exceptions[teamCount];
+  }
+
+  let nearestSixteen = 16;
+  while (nearestSixteen < teamCount) {
+      nearestSixteen *= 2;
+  }
+  nearestSixteen = Math.floor(nearestSixteen/2);
+  layout[1] = teamCount - (nearestSixteen % teamCount);
+  round += 1;
+  nearestSixteen = Math.floor(nearestSixteen/2);
+  while (nearestSixteen >= 1) {
+      layout[round] = nearestSixteen;
+      nearestSixteen = Math.floor(nearestSixteen/2);
+      round += 1;
+  }
+  return layout
+}
 
 class Bracket extends Component {
   state = {};
