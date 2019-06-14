4b539f0a91ff8b8ffab054451328305e1e975830
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-06-13 22:44:01 -0400

    Finish consolidating styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Sandbox from './components/Sandbox';
+import './styles.scss';
 import * as serviceWorker from './serviceWorker';
 
 const root = document.getElementById('root');
